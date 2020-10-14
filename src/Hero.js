971fa8ef4fbc4372a4812f02d059917fe013ed35
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-10-14 11:40:11 -0400

    Initial commit

---
+++ src/Hero.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Hero.css';
+
+class Hero extends Component {
+    render() {
+        return (
+            <div id='hero'>
+                <div className='card'>
+                    <h1>Amélégumes</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Hero;
