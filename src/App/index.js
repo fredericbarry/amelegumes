979833d9fd247fbb21f87637b6372dabb582c17e
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-10-14 22:42:04 -0400

    Created a folder structure

---
+++ src/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Hero from "../Hero/index.js";
+
+class App extends Component {
+  render() {
+    return <Hero />;
+  }
+}
+
+export default App;
---
+++ src/Hero/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Hero extends Component {
+  render() {
+    return (
+      <div id="hero">
+        <div className="header">
+          <h1>Amélégumes</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
