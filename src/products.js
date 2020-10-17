aba9c348c4f1891ea75fb5389491b5d24dfb2ceb
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-10-16 23:40:18 -0400

    Added flat file database

---
+++ src/products.js
@@ -0,0 +1,26 @@
+export const products = [
+    {
+        name: 'Ace 55',
+        image: 'ace-55.jpg',
+        category: 'tomatoes',
+        description: 'L’Ace 55 est robuste, résilient et mieux servi tout juste sorti du jardin. Faible taux d’acidité.',
+        isDeterminate: true,
+        isFavorite: false
+    },
+    {
+        name: 'Big Beef',
+        image: 'big-beef.jpg',
+        category: 'tomatoes',
+        description: 'Grosses tomates, la favorite des grands consommateurs. Tout est dans le nom. Gros fruits «costauds» de 10 à 12 oz qui ont une excellente saveur pour une grosse tomate. Produit jusqu’au gel.',
+        isDeterminate: false,
+        isFavorite: false
+    },
+    {
+        name: 'Millefleur',
+        image: 'millefleur.jpg',
+        category: 'tomatoes',
+        description: 'Une étonnante variété multiflora aux grappes énormes couvertes d’une myriade de fleurs, d’où la variété tire évidemment son nom. Une seule grappe peut ensuite contenir jusqu’à 150 tomates! Petits fruits de forme ronde à ovale (2-3 cm), de couleur jaune doré. Bonne saveur fruitée.',
+        isDeterminate: false,
+        isFavorite: true
+    },
+];
