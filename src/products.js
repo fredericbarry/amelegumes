7ba00488d7b0bc04e6b72853f28bc65e226b9ba3
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-10-22 20:19:11 -0400

    Improved product design

--- src/products.js
+++ src/products.js
@@ -1,77 +1,96 @@
 export const products = [
   {
+    id: 1,
     name: "Big Beef",
     image: "big-beef.jpg",
   },
   {
+    id: 2,
     name: "Jasper",
     image: "jasper.jpg",
   },
   {
+    id: 3,
     name: "Roma",
     image: "roma.jpg",
   },
   {
+    id: 4,
     name: "Blue Cream",
     image: "blue-cream.jpg",
   },
   {
+    id: 5,
     name: "Principe Borghese",
     image: "principe-borghese.jpg",
   },
   {
+    id: 6,
     name: "Beefsteak",
     image: "beefsteak.jpg",
   },
   {
+    id: 6,
     name: "Black Cherry",
     image: "black-cherry.jpg",
   },
   {
+    id: 8,
     name: "Taxi",
     image: "taxi.jpg",
   },
   {
+    id: 9,
     name: "Millefleur",
     image: "millefleur.jpg",
   },
   {
+    id: 10,
     name: "Candyland",
     image: "candyland.jpg",
   },
   {
+    id: 11,
     name: "Sungold",
     image: "sungold.jpg",
   },
   {
+    id: 12,
     name: "Black Krim",
     image: "black-krim.jpg",
   },
   {
+    id: 13,
     name: "Ace 55",
     image: "ace-55.jpg",
   },
   {
+    id: 14,
     name: "Brandywine",
     image: "brandywine.jpg",
   },
   {
+    id: 15,
     name: "Green Zebra",
     image: "green-zebra.jpg",
   },
   {
+    id: 16,
     name: "San Marzano",
     image: "san-marzano.jpg",
   },
   {
+    id: 17,
     name: "Chef's Choice Orange",
     image: "chefs-choice-orange.jpg",
   },
   {
+    id: 18,
     name: "Clementine",
     image: "clementine.jpg",
   },
   {
+    id: 19,
     name: "Pink Cupcake",
     image: "pink-cupcake.jpg",
   },
