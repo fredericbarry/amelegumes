3e837e5bcb043cf4fc56ddc2a047aa525f41043d
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-11-06 20:08:14 -0500

    Use functional components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,25 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
 
-class Product extends Component {
-  render() {
-    return (
-      <Card>
-        <CardMedia
-          component="img"
-          alt={this.props.name}
-          image={
-            process.env.PUBLIC_URL + "/images/products/" + this.props.image
-          }
-        />
-        <CardContent>
-          <Typography component="h2" variant="h6" color="primary">
-            {this.props.name}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-  }
-}
+const Product = (props) => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        alt={props.name}
+        image={process.env.PUBLIC_URL + "/images/products/" + props.image}
+      />
+      <CardContent>
+        <Typography component="h2" variant="h6" color="primary">
+          {props.name}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Product;
