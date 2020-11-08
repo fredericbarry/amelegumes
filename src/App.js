0575763c362535ae84ef3e6b1213611bc01d13fe
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-11-07 20:17:30 -0500

    Add blank lines

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
+
 import { products } from "./products";
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import "fontsource-roboto";
 import "./index.css";
 
 import App from "./App";
+
 import * as serviceWorker from "./serviceWorker";
 
 const theme = createMuiTheme({
