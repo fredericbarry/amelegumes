d6bfa27e7dfacc6e1f9c267cf8301d34cc1f858b
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-11-09 15:27:27 -0500

    Added products CSS

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Box, Grid } from "@material-ui/core";
+
+import "./Products.css";
+
 import Product from "../Product/Product";
 
 const Products = ({ products }) => {
   return (
-    <Box p={2}>
-      <Grid container spacing={1}>
+    <section className="ProductsSection">
+      <div className="ProductsGrid">
         {products.map((product, i) => {
           return (
-            <Grid item key={products[i].id} xs={12} sm={6} md={4} lg={3} xl={1}>
-              <Product
-                name={products[i].name}
-                image={products[i].image}
-                description={products[i].description}
-              />
-            </Grid>
+            <Product
+              key={products[i].id}
+              name={products[i].name}
+              image={products[i].image}
+              description={products[i].description}
+            />
           );
         })}
-      </Grid>
-    </Box>
+      </div>
+    </section>
   );
 };
 
