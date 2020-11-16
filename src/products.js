ebce8f9cfae9615d0166cae611890a615bc287be
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-11-16 17:54:36 -0500

    Added product descriptions

--- src/products.js
+++ src/products.js
@@ -4,102 +4,129 @@ export const products = [
     name: "Tomate Big Beef",
     category: "Tomates",
     image: "big-beef.jpg",
-    isDeterminate: false,
+    description:
+      "Grosses tomates, la favorite des grands consommateurs. Tout est dans le nom. Gros fruits «costauds» de 10 à 12 oz qui ont une excellente saveur pour une grosse tomate. Produit jusqu’au gel.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 2,
     name: "Tomate Jasper",
     category: "Tomates",
     image: "jasper.jpg",
-    isDeterminate: false,
+    description:
+      "Petite cerise rouge foncé. Ronde, 7–10 g. La texture est agréablement moelleuse et la saveur est douce et riche. Les plantes sont très vigoureuses et hautes et restent en bonne santé pendant une longue période de cueillette. Gagnante AAS.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 3,
     name: "Tomate Roma",
     category: "Tomates",
     image: "roma.jpg",
-    isDeterminate: true,
+    description:
+      "Une excellente tomate à pollinisation libre de type pâte. Populaire pour la mise en conserve ou pour faire votre propre sauce tomate, jus, soupes ou pour déshydrater. Les plants portent des dizaines de fruits de 6 oz en forme de prune, qui ont des intérieurs charnus et très peu de graines.",
     isFavorite: false,
+    isDeterminate: true,
   },
   {
     id: 4,
     name: "Tomate Blue Cream",
     category: "Tomates",
     image: "blue-cream.jpg",
-    isDeterminate: false,
+    description:
+      "Une tomate cerise super sucrée et prolifique. Saveur délicate mais complexe et une belle couleur crème avec des éclaboussures bleu violet.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 5,
     name: "Tomate Principe Borghese",
     category: "Tomates",
     image: "principe-borghese.jpg",
-    isDeterminate: true,
+    description:
+      "La tomate Principe Borghese est une variété cultivée en Italie pour le séchage au soleil. Choisissez une grappe entière et accrochez-la contre un mur chaud et ensoleillé pour la sécher ou coupez-la en deux et utilisez un déshydrateur. Les tomates séchées au soleil peuvent être mises en conserve ou congelées et conserver leur saveur et leurs qualités nutritionnelles. Cette variété fait également une excellente pâte de tomate pour la mise en conserve ou une utilisation fraîche. Essayez d'en faire pousser dans des pots de trois à cinq gallons sur le balcon ou contre le mur sud d'une maison pour maximiser la chaleur du soleil.",
     isFavorite: false,
+    isDeterminate: true,
   },
   {
     id: 6,
     name: "Tomate Beefsteak",
     category: "Tomates",
     image: "beefsteak.jpg",
-    isDeterminate: false,
+    description:
+      "Tout est dans le nom! La Beefsteak produit des rendements élevés de gros fruits juteux de 10 à 12 oz «costaud» qui ont une excellente saveur. Produit jusqu'au gel. Une tranche de cette beauté remplira votre sandwich à la tomate. De plus, cette variété est un excellent ajout aux salades et autres plats.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 7,
     name: "Tomate Black Cherry",
     category: "Tomates",
     image: "black-cherry.jpg",
-    isDeterminate: false,
+    description:
+      "Douce et robuste. Ronde, 15-20 g. Les fruits sont presque noirs. La saveur est dynamique — un peu comme un héritage. Rendement élevé.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 8,
     name: "Tomate Taxi",
     category: "Tomates",
     image: "taxi.jpg",
-    isDeterminate: true,
+    description:
+      "Excellente variété jaune, hâtive, bien adaptée aux saisons courtes. Beaux fruits de couleur jaune profond, de 4 à 5 cm de diamètre, de forme ronde à ovale. Tomates parfaites, lisses et sans défaut. Pas de craquement. Saveur très agréable, pas insipide du tout. Très bon goût rafraîchissant. Petits plants compacts de 70 cm. Bon potentiel commercial. Sans contredit, l'une des meilleures tomates jaunes: belle, bonne et hâtive!",
     isFavorite: false,
+    isDeterminate: true,
   },
   {
     id: 9,
     name: "Tomate Millefleur",
     category: "Tomates",
     image: "millefleur.jpg",
-    isDeterminate: false,
+    description:
+      "Une étonnante variété multiflora aux grappes énormes couvertes d’une myriade de fleurs, d’où la variété tire évidemment son nom. Une seule grappe peut ensuite contenir jusqu’à 150 tomates! Petits fruits de forme ronde à ovale (2-3 cm), de couleur jaune doré. Bonne saveur fruitée.",
     isFavorite: true,
+    isDeterminate: false,
   },
   {
     id: 10,
     name: "Tomate Candyland",
     category: "Tomates",
     image: "candyland.jpg",
-    isDeterminate: false,
+    description:
+      "Gagnante AAS. Tomate groseille à maturation précoce. Cette plante hybride indéterminée produit un rendement très élevé de plus de 100 fruits minuscules, (½ pouce) rouges, super sucrés, ¼ oz avec une saveur concentrée.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 11,
     name: "Tomate Sungold",
     category: "Tomates",
     image: "sungold.jpg",
-    isDeterminate: false,
+    description:
+      "Saveur fruitée intense. Les tomates cerises orange mandarine exceptionnellement douces et brillantes laissent tout le monde en redemander. Les plantes vigoureuses commencent à produire tôt et produisent tout au long de la saison. Le goût est imbattable. Fruits de 15-20 grammes.",
     isFavorite: true,
+    isDeterminate: false,
   },
   {
     id: 12,
     name: "Tomate Black Krim",
     category: "Tomates",
     image: "black-krim.jpg",
-    isDeterminate: false,
+    description:
+      "Héritage russe. Black Krim combine une saveur fumé audacieuse et une bonne texture avec une apparence inhabituelle. Brun foncé / rouge. Les tomates de 8-16 onces ont des épaules brunes / vertes qui s'assombrissent avec plus de chaleur et de soleil.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 13,
     name: "Tomate Ace 55",
     category: "Tomates",
     image: "ace-55.jpg",
+    description:
+      "L’Ace 55 est robuste, résilient et mieux servi tout juste sorti du jardin. Faible taux d’acidité.",
+    isFavorite: false,
     isDeterminate: true,
   },
   {
@@ -107,54 +134,68 @@ export const products = [
     name: "Tomate Brandywine",
     category: "Tomates",
     image: "brandywine.jpg",
-    isDeterminate: false,
+    description:
+      "La norme pour la saveur de tomate traditionnelle. Bien connu pour sa taille et sa saveur exotique et sucrée de tomate. La croissance compacte et indéterminée produit de gros fruits de 1 lb à chair rouge rosée.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 15,
     name: "Tomate Green Zebra",
     category: "Tomates",
     image: "green-zebra.jpg",
-    isDeterminate: false,
+    description:
+      "Une tomate populaire parmi les chefs gastronomiques, les marchés spécialisés et les jardiniers amateurs. Cette tomate est douce, riche et a une saveur acidulée. Cette tomate verte avec des rayures vert lime vif fait un bel ajout à n'importe quel plat. Les rayures passeront du blanc au jaune à pleine maturité.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 16,
     name: "Tomate San Marzano",
     category: "Tomates",
     image: "san-marzano.jpg",
-    isDeterminate: false,
+    description:
+      "La tomate San Marzano est la reine incontestée des tomates «pâte» . Chaque plante produit des quantités insensées de tomates moyennes de 3 à 5 onces. Attendez-vous à une production élevée jusqu'au premier gel. La saveur est acide; la rendant idéale pour la mise en conserve, la pâte de tomate, la sauce à pizza, la sauce pour pâtes et même fraîche dans les salades. Les chefs gastronomiques et amateurs recherchent la tomate San Marzano pour sa saveur incroyable et sa texture parfaite.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 17,
     name: "Tomate Chef's Choice Orange",
     category: "Tomates",
     image: "chefs-choice-orange.jpg",
-    isDeterminate: false,
+    description:
+      "Beefsteak orange attrayante. Gagnante AAS. Les fruits de 8-11 onces sont peu acides et ont une très bonne saveur et texture. Rendement élevé. Plus hâtive et plus productive que la plupart des beefsteak ​​orange.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 18,
     name: "Tomate Clementine",
     category: "Tomates",
     image: "clementine.jpg",
-    isDeterminate: false,
+    description:
+      "Tomate cocktail orange unique. Fruit rond ovale d’en moyenne 2 onces. Les plantes produisent des rendements élevés avec une saveur attrayante et acidulée. Exceptionnelle lorsqu'elle est coupée en deux et rôtie.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 19,
     name: "Tomate Pink Cupcake",
     category: "Tomates",
     image: "pink-cupcake.jpg",
-    isDeterminate: true,
+    description:
+      "Tomate rose hâtive, sans épaule verte, très ferme et excellente au goût.",
     isFavorite: false,
+    isDeterminate: true,
   },
   {
     id: 20,
     name: "Concombre Summer Dance",
     category: "Concombres",
     image: "summer-dance.jpg",
+    description:
+      "Très long à peau fine. Concombres incroyablement croquants et lisses. Summer Dance est l'un des concombres les plus savoureux et frais. Il produit de gros rendements de fruits vert foncé uniformes qui poussent jusqu'à 10 pouces de long. Très droit pour un concombre aussi long. Tolérant au stress thermique.",
     isFavorite: false,
   },
   {
@@ -162,6 +203,8 @@ export const products = [
     name: "Concombre Marketmore 76",
     category: "Concombres",
     image: "marketmore-76.jpg",
+    description:
+      "Les plantes produisent des concombres verts foncés, croquants et au goût délicieux, d'une longueur de 8 à 9 pouces.",
     isFavorite: false,
   },
   {
@@ -169,6 +212,8 @@ export const products = [
     name: "Concombre National Pickling",
     category: "Concombres",
     image: "national-pickling.jpg",
+    description:
+      "Développée spécialement par la National Pickle Packers Association, cette variété a été conçue pour s'intégrer parfaitement dans un pot de cornichon! Cette variété produit une abondance de concombres de 5 à 7 pouces. Mangez-les frais, en salade ou en jus. Lorsqu'ils sont transformés en cornichons à l'aneth ou en ‘’bread and butter’’, leur saveur et leur croquant sont hors de ce monde. Peu de graines, et les graines produites sont petites et n'affectent pas la texture.",
     isFavorite: false,
   },
   {
@@ -176,6 +221,8 @@ export const products = [
     name: "Cerise de terre Aunt Molly’s",
     category: "Fruits",
     image: "aunt-mollys.jpg",
+    description:
+      "Délicieux goût tropical dans un joli emballage en papier. Les cerises de terre sont très sucrées et ont un peu le goût de l'ananas. Peut être utilisé dans des conserves, des tartes ou sur des glaces, mais ici, nous adorons les manger crus.",
     isFavorite: false,
   },
   {
@@ -183,6 +230,8 @@ export const products = [
     name: "Basilic Genovese",
     category: "Herbes",
     image: "genovese-basil.jpg",
+    description:
+      "D’Italie, cette variété a une saveur puissante qui est idéale pour faire du pesto frais. Facile à cultiver en pot. Pousse jusqu'à 2 pieds de haut. N’oubliez pas de récolter leurs fleurs comestibles. Profil gustatif: très aromatique, parfait pour manger frais et ajouter comme garniture aux plats, en particulier à la cuisine italienne. Apparence: belle plante luxuriante, idéale pour compagnon à côté des tomates.",
     isFavorite: false,
   },
   {
@@ -190,6 +239,8 @@ export const products = [
     name: "Basilic Siam Queen",
     category: "Herbes",
     image: "siam-queen-basil.jpg",
+    description:
+      "Cette variété tendre est intensément parfumée avec un goût de réglisse et de basilic. Les feuilles robustes conservent leur forme et leur goût à des températures de cuisson élevées ou prolongées. Parfait dans les recettes thaïlandaises. Profil gustatif: goût de réglisse avec une bouchée d'anis épicé. Apparence: les feuilles peuvent atteindre 4 pouces de long et 2 pouces de large avec des pousses de fleurs violet foncé frappantes.",
     isFavorite: false,
   },
   {
@@ -197,6 +248,8 @@ export const products = [
     name: "Basilic Licorice",
     category: "Herbes",
     image: "licorice-basil.jpg",
+    description:
+      "Aussi appelé basilic anis, le basilic Licorice doit son nom à sa saveur forte mais agréable. Les feuilles atteindront un vert vif avec des accents violets, et ses fleurs sont également comestibles.",
     isFavorite: false,
   },
   {
@@ -204,6 +257,8 @@ export const products = [
     name: "Basilic Cinnamon",
     category: "Herbes",
     image: "cinnamon-basil.jpg",
+    description:
+      "Cette variété doit son nom à ses tiges couleur cannelle et à sa saveur épicée chaude. Délicieux avec des fruits et dans des plats asiatiques ou indiens.",
     isFavorite: false,
   },
   {
@@ -211,6 +266,8 @@ export const products = [
     name: "Basilic Freddy Red",
     category: "Herbes",
     image: "freddy-red-basil.jpg",
+    description:
+      "Excellente saveur et couleur accrocheuse. Les mêmes performances fortes et la même saveur traditionnelle que les autres Basils de type Genovese que nous adorons tous, mais avec l'attrait supplémentaire du feuillage rouge foncé à marron.",
     isFavorite: false,
   },
   {
@@ -218,6 +275,8 @@ export const products = [
     name: "Sauge",
     category: "Herbes",
     image: "sauge.jpg",
+    description:
+      "Feuilles grises utilisées fraîches ou séchées avec de la viande ou dans des farces.",
     isFavorite: false,
   },
   {
@@ -225,6 +284,8 @@ export const products = [
     name: "Zucchini Black Beauty",
     category: "Courges",
     image: "black-beauty-zucchini.jpg",
+    description:
+      "Courgettes vert foncé, brillantes, droites et minces. Peut atteindre la taille d'une batte de baseball ou plus si vous l'oubliez! La texture est ferme et très tendre avec une saveur incroyable. Idéal à ajouter à vos recettes grillées telles que les brochettes ou à incorporer dans du pain aux courgettes pour une gâterie sucrée.",
     isFavorite: false,
   },
   {
@@ -232,6 +293,8 @@ export const products = [
     name: "Zucchini Cocozelle",
     category: "Courges",
     image: "cocozelle-zucchini.jpg",
+    description:
+      "Si vous aimez cultiver des légumes géants, celui-ci est fait pour vous. La taille moyenne de la courge est d'environ 45 livres lorsqu'elle est à pleine maturité. Cependant, dans son Italie natale, cette variété est récoltée très tôt lorsque les fruits mesurent moins de 8 pouces de longueur pour une meilleure saveur. Lorsqu’il est petit, il est tendre, délicieux et pas du tout filandreux. Cette délicieuse courge d'été est idéale pour les grillades. Récoltez tôt pour prolonger la récolte sur chaque plante.",
     isFavorite: false,
   },
   {
@@ -239,6 +302,8 @@ export const products = [
     name: "Zucchini Italian Striped",
     category: "Courges",
     image: "italian-striped-zucchini.jpg",
+    description:
+      "Un producteur prolifique. Cet héritage italien a un motif distinct et une saveur de noisette. Les plantes produisent des courgettes plus petites que les autres variétés, mais compensent en rendements. Parfait pour le jardinier de petit espace. La plante est également plus tolérante aux maladies et dure plus tard dans la saison que la plupart des autres variétés. Merveilleuses sautés ou rôtis.",
     isFavorite: false,
   },
   {
@@ -246,6 +311,8 @@ export const products = [
     name: "Zucchini Spineless Perfection",
     category: "Courges",
     image: "spineless-perfection-zucchini.jpg",
+    description:
+      "Les fruits attrayants et droits sont portés sur une plante ouverte et sans épines, ce qui facilite la récolte. Les résistances multiples aux maladies maintiennent les plantes en vie pendant une période prolongée.",
     isFavorite: false,
   },
   {
@@ -253,6 +320,8 @@ export const products = [
     name: "Zucchini Eight Ball",
     category: "Courges",
     image: "eight-ball-zucchini.jpg",
+    description:
+      "Cette variété est une courgette ronde facile à cultiver, idéale pour les petits jardins. Les plantes compactes produisent une récolte généreuse tout au long de la saison. Étant un légume si polyvalent, la courge à “Eight Ball” peut être utilisée crue, cuite au four, frite, cuite à la vapeur, farcie et a une excellente saveur. Récoltez jeune pour des fruits tendres.",
     isFavorite: false,
   },
   {
@@ -260,6 +329,8 @@ export const products = [
     name: "Courge Spaghetti",
     category: "Courges",
     image: "spaghetti-squash.jpg",
+    description:
+      "L’alternative classique aux pâtes. La chair de cette excellente variété à la meilleure consistance semblable à celle des nouilles. Saveur de noisette légèrement sucrée. De forme oblongue avec une peau ivoire qui vire au jaune pâle à maturité. Vignes courtes.",
     isFavorite: false,
   },
   {
@@ -267,6 +338,8 @@ export const products = [
     name: "Courge Butternut Waltham",
     category: "Courges",
     image: "waltham-butternut.jpg",
+    description:
+      "Courge musquée beige avec peau mince et chaire orange. Devient sucrée en entreposage. Elle se conserve tout l’hiver à 15ºC.",
     isFavorite: false,
   },
   {
@@ -274,14 +347,18 @@ export const products = [
     name: "Tomate Blush",
     category: "Tomates",
     image: "blush.jpg",
-    isDeterminate: false,
+    description:
+      "Belle tomate à grignoter jaune vif. Des rayures rouges apparaissent au fur et à mesure du développement. Les plantes tolèrent des conditions difficiles. Fruits de 25-30 grammes.",
     isFavorite: false,
+    isDeterminate: false,
   },
   {
     id: 38,
     name: "Tomatillo Rio Grande Verde",
     category: "Tomatillos",
     image: "rio-grande-verde-tomatillo.jpg",
+    description:
+      "La plus grosse des variétés de tomatillos sur le marché avec des fruits atteignant 3-4 onces. Le tomatillo de choix pour la salsa verde maison avec sa saveur douce et lumineuse de poivron vert mélangé à de la tomate. Les plantes produisent de gros rendements de petits fruits recouverts de cosses qui tombent au sol à pleine maturité, ce qui ne nécessite pas de tuteur.",
     isFavorite: false,
   },
   {
@@ -289,6 +366,8 @@ export const products = [
     name: "Tomatillo Mauve",
     category: "Tomatillos",
     image: "purple-tomatillo.jpg",
+    description:
+      "Cette variété de tomatillo donne une salsa verde plus douce et acidulée. Le fruit devient violet foncé à maturité et la couleur vive est présente sur tout le fruit. Plus doux que les tomatillos vertes. À utiliser pour la mise en conserve, le rôtissage ou les grillades. Les gros fruits peuvent atteindre jusqu'à 1 à 1 ½ pouces chacun. Les plantes atteignent jusqu'à 10 pouces de haut, 24 pouces de large et poussent bien sans tuteur.",
     isFavorite: false,
   },
   {
@@ -296,6 +375,8 @@ export const products = [
     name: "Aubergine Patio Baby",
     category: "Aubergines",
     image: "patio-baby-eggplant.jpg",
+    description:
+      "Variété unique d’aubergine au port compact, avec très peu d’épines, produisant des fruits miniatures, hâtifs, que les jardiniers peuvent récolter sur une longue période. L’aubergine Patio Baby se cultive tant en pot qu’en pleine terre. Gagnant All America Selection.",
     isFavorite: false,
   },
 ];
