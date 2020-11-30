59b9d7c8c386e1ecaa3dc8550c75a50306619fe9
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-11-29 22:25:02 -0500

    Add padding to the NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,12 @@ import React from "react";
 import "./NavBar.css";
 
 const NavBar = (props) => {
-  return <nav className="NavBar">{props.children}</nav>;
+  return (
+    <>
+      <nav className="NavBar">{props.children}</nav>
+      <div className="NavBarPadding"></div>
+    </>
+  );
 };
 
 export default NavBar;
