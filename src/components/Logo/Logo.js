1cdf238d48fa427b57d8be3615913e179fe3b5e2
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-06 14:25:04 -0500

    Refactor Logo component to use SCSS

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import "./Logo.css";
+import "./Logo.scss";
 
 const Logo = ({ className }) => {
-  return <div className={`LogoText ${className}`}>Amélégumes</div>;
+  return <div className={`logo ${className}`}>Amélégumes</div>;
 };
 
 export default Logo;
