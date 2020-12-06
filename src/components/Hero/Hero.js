af825d916cd10c8b7bd021122d4c1f9eea1f96b8
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-06 14:06:29 -0500

    Refactor Hero component to use SCSS

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-import "./Hero.css";
+import "./Hero.scss";
 
 import Logo from "../Logo/Logo";
 
 const Hero = () => {
   return (
-    <section className="HeroSection">
-      <div className="HeroContent">
-        <Logo className="HeroLogo" />
-        <div className="HeroDescription">
+    <div className="hero">
+      <div className="content">
+        <Logo className="logo" />
+        <div className="description">
           Les légumes les plus Amélicieux qui soient
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
