c948dd5fd414d50457c475f2f5a8f4c604719998
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-06 14:38:35 -0500

    Refactor Products component to use SCSS

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,43 +1,26 @@
 import React from "react";
 
-import "./Products.css";
+import "./Products.scss";
 
 import Product from "../Product/Product";
 
 const Products = ({ products }) => {
   return (
-    <section className="ProductsSection">
-      <div className="ProductsGrid">
-        {
-          products.map((product) => {
-            return (
-              <Product
-                key={product.id}
-                name={product.name}
-                image={product.image}
-                price={product.price}
-                description={product.description}
-              />
-            );
-          })
-          /*
-          Object.entries(categories).map(([category, products]) => {
-          return products.map((product) => {
-            return (
-              <Product
-                key={product.id}
-                name={product.name}
-                image={product.image}
-                price={product.price}
-                description={product.description}
-              />
-            );
-          });
-        })
-        */
-        }
+    <div className="products">
+      <div className="grid">
+        {products.map((product) => {
+          return (
+            <Product
+              key={product.id}
+              name={product.name}
+              image={product.image}
+              price={product.price}
+              description={product.description}
+            />
+          );
+        })}
       </div>
-    </section>
+    </div>
   );
 };
 
