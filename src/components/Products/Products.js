0485a656631efd1dcd270048c4bdb263269786fd
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-09 23:33:17 -0500

    Extract ProductList from Products component

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,23 +2,13 @@ import React from "react";
 
 import "./Products.scss";
 
-import Product from "../Product/Product";
+import ProductList from "../ProductList/ProductList";
 
 const Products = ({ products }) => {
   return (
     <div className="products">
       <div className="grid">
-        {products.map((product) => {
-          return (
-            <Product
-              key={product.id}
-              name={product.name}
-              image={product.image}
-              price={product.price}
-              description={product.description}
-            />
-          );
-        })}
+        <ProductList products={products} />
       </div>
     </div>
   );
