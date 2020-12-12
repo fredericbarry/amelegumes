da54b031cbd50098c546fbb0a471225422b39d2a
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-12 13:31:46 -0500

    Add icon to search box

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import IconSearch from "../../icons/IconSearch";
+
 import "./SearchBox.scss";
 
 const SearchBox = ({ placeholder, onChange }) => {
   return (
     <div className="search-box">
+      <IconSearch />
       <input type="search" placeholder={placeholder} onChange={onChange} />
     </div>
   );
