32843cb884853254f7d35eacc47ad668b94498d3
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2020-12-12 17:34:39 -0500

    Move products logic, display no results found msg

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,16 +2,84 @@ import React from "react";
 
 import "./Products.scss";
 
+import Loader from "../Loader/Loader";
+import ProductFilters from "../ProductFilters/ProductFilters";
 import ProductList from "../ProductList/ProductList";
+import SearchBox from "../SearchBox/SearchBox";
 
-const Products = ({ products }) => {
-  return (
-    <div className="products">
-      <div className="grid">
-        <ProductList products={products} />
+class Products extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      searchInput: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/fredericbarry/amelegumes-api/master/db.json"
+    )
+      .then((response) => response.json())
+      .then((products) => this.setState({ products: products }));
+  }
+
+  handleSearchBoxChange = (event) => {
+    this.setState({ searchInput: event.target.value });
+  };
+
+  render() {
+    const { products, searchInput } = this.state;
+    const filteredProducts = products
+      .sort((a, b) => {
+        // If category is the same then sort by name
+        if (a.category === b.category) {
+          return a.name > b.name ? 1 : -1;
+        }
+        return a.category > b.category ? 1 : -1;
+      })
+      .filter((product) => {
+        return (
+          product.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+          product.category.toLowerCase().includes(searchInput.toLowerCase()) ||
+          product.description.toLowerCase().includes(searchInput.toLowerCase())
+        );
+      });
+
+    return (
+      <div className="products">
+        {!products.length ? (
+          <Loader />
+        ) : (
+          <>
+            <ProductFilters>
+              <SearchBox
+                placeholder="Rechercher"
+                onChange={this.handleSearchBoxChange}
+              />
+            </ProductFilters>
+            {!filteredProducts.length ? (
+              <div className="no-results-found">
+                <div className="emoji">🤷‍♀️</div>
+                <p>
+                  Désolée, je ne trouve pas de <q>{searchInput}</q>&nbsp;…
+                </p>
+                <p>
+                  On va se le dire, mon moteur de recherche est du genre un peu
+                  simplet, donc soyez pas trop compliqué dans votre choix de mot
+                  pour pas trop le mêler, vous savez! 😉
+                </p>
+              </div>
+            ) : (
+              <div className="grid">
+                <ProductList products={filteredProducts} />
+              </div>
+            )}
+          </>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Products;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,70 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Hero from "../components/Hero/Hero";
-import Loader from "../components/Loader/Loader";
 import Navbar from "../components/Navbar/Navbar";
-import ProductFilters from "../components/ProductFilters/ProductFilters";
 import Products from "../components/Products/Products";
-import SearchBox from "../components/SearchBox/SearchBox";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [],
-      searchInput: "",
-    };
-  }
-
-  componentDidMount() {
-    fetch(
-      "https://raw.githubusercontent.com/fredericbarry/amelegumes-api/master/db.json"
-    )
-      .then((response) => response.json())
-      .then((products) => this.setState({ products: products }));
-  }
-
-  handleSearchBoxChange = (event) => {
-    this.setState({ searchInput: event.target.value });
-  };
-
-  render() {
-    const { products, searchInput } = this.state;
-    const filteredProducts = products
-      .sort((a, b) => {
-        // If category is the same then sort by name
-        if (a.category === b.category) {
-          return a.name > b.name ? 1 : -1;
-        }
-        return a.category > b.category ? 1 : -1;
-      })
-      .filter((product) => {
-        return (
-          product.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-          product.category.toLowerCase().includes(searchInput.toLowerCase()) ||
-          product.description.toLowerCase().includes(searchInput.toLowerCase())
-        );
-      });
-    return (
-      <>
-        <Navbar />
-        <Hero />
-        {!products.length ? (
-          <Loader />
-        ) : (
-          <>
-            <ProductFilters>
-              <SearchBox
-                placeholder="Rechercher"
-                onChange={this.handleSearchBoxChange}
-              />
-            </ProductFilters>
-            <Products products={filteredProducts} />
-          </>
-        )}
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Products />
+    </>
+  );
+};
 
 export default App;
