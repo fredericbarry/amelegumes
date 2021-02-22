1924433d552523e0930f9970fecc615fb3b4575d
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2021-02-21 20:34:49 -0500

    Refactor component

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Component, useEffect, useState } from "react";
 
 import "./Products.scss";
 
@@ -7,38 +7,44 @@ import ProductFilters from "../ProductFilters/ProductFilters";
 import ProductList from "../ProductList/ProductList";
 import SearchBox from "../SearchBox/SearchBox";
 
-/** BEGIN **/
+const Products = () => {
+  const [isFetching, setIsFetching] = useState(true);
+  const [products, setProducts] = useState(null);
 
-// https://www.itcreativelabs.com/blog/connect-to-woocommerce-rest-api-using-javascript/
+  const basicAuth = (key, secret) => {
+    let hash = btoa(key + ":" + secret);
+    return "Basic " + hash;
+  };
 
-const wooClientKey = "ck_ec50580d8815bd2b14430d875fab0830a26b4aa4";
-const wooClientSecret = "cs_f081ccd5aa3230d9a1c8eb1ce564338e491e10fc";
-
-function basicAuth(key, secret) {
-  let hash = btoa(key + ":" + secret);
-  return "Basic " + hash;
-}
-
-let auth = basicAuth(wooClientKey, wooClientSecret);
-
-async function getProducts() {
-  try {
-    const response = await fetch(
-      "https://backend.amelegumes.com/wp-json/wc/v3/products",
+  useEffect(() => {
+    fetch(
+      process.env.REACT_APP_API_DOMAIN_URL +
+        "wp-json/wc/v3/products?per_page=100&status=publish",
       {
-        headers: { Authorization: basicAuth(wooClientKey, wooClientSecret) },
+        headers: {
+          Authorization: basicAuth(
+            process.env.REACT_APP_API_CLIENT_KEY,
+            process.env.REACT_APP_API_CLIENT_SECRET
+          ),
+        },
       }
-    );
-    return await response.json();
-  } catch (error) {
-    // catches errors both in fetch and response.json
-    console.log(error);
-  }
-}
+    )
+      .then((res) => res.json())
+      .then((products) => {
+        setProducts(products);
+        setIsFetching(false);
+      });
+  }, []);
 
-//** END **//
+  return (
+    <>
+      {isFetching && <Loader />}
+      {products && <ProductList products={products} />}
+    </>
+  );
+};
 
-class Products extends React.Component {
+class oldProducts extends Component {
   constructor() {
     super();
     this.state = {
@@ -47,77 +53,44 @@ class Products extends React.Component {
     };
   }
 
-  fetchGitHubProducts() {
-    fetch(
-      "https://raw.githubusercontent.com/fredericbarry/amelegumes-api/master/db.json"
-    )
-      .then((response) => response.json())
-      .then((products) => this.setState({ products: products }));
-  }
-
-  fetchWooCommerceProducts() {
-    fetch("https://backend.amelegumes.com/wp-json/wc/v3/products")
-      .then((response) => response.json())
-      .then((products) => this.setState({ products: products }));
-  }
-
-  componentDidMount() {
-    this.fetchGitHubProducts();
-  }
-
   handleSearchBoxChange = (event) => {
     this.setState({ searchInput: event.target.value });
   };
 
   render() {
     const { products, searchInput } = this.state;
-    const filteredProducts = products
-      .sort((a, b) => {
-        // If category is the same then sort by name
-        if (a.category === b.category) {
-          return a.name > b.name ? 1 : -1;
-        }
-        return a.category > b.category ? 1 : -1;
-      })
-      .filter((product) => {
-        return (
-          product.name.toLowerCase().includes(searchInput.toLowerCase()) ||
-          product.category.toLowerCase().includes(searchInput.toLowerCase()) ||
-          product.description.toLowerCase().includes(searchInput.toLowerCase())
-        );
-      });
+    const filteredProducts = products.filter((product) => {
+      return (
+        product.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    });
 
     return (
       <div className="products">
-        {!products.length ? (
-          <Loader />
-        ) : (
-          <>
-            <ProductFilters>
-              <SearchBox
-                placeholder="Rechercher"
-                onChange={this.handleSearchBoxChange}
-              />
-            </ProductFilters>
-            {!filteredProducts.length ? (
-              <div className="no-results-found">
-                <div className="emoji">🤷‍♀️</div>
-                <p>
-                  Désolée, je ne trouve pas de <q>{searchInput}</q>&nbsp;…
-                </p>
-                <p>
-                  On va se le dire, mon moteur de recherche est du genre un peu
-                  simplet, donc soyez pas trop compliqué dans votre choix de mot
-                  pour ne pas trop le mêler! 😉
-                </p>
-              </div>
-            ) : (
-              <div className="grid">
-                <ProductList products={filteredProducts} />
-              </div>
-            )}
-          </>
-        )}
+        <>
+          <ProductFilters>
+            <SearchBox
+              placeholder="Rechercher"
+              onChange={this.handleSearchBoxChange}
+            />
+          </ProductFilters>
+          {!filteredProducts.length ? (
+            <div className="no-results-found">
+              <div className="emoji">🤷‍♀️</div>
+              <p>
+                Désolée, je ne trouve pas de <q>{searchInput}</q>&nbsp;…
+              </p>
+              <p>
+                On va se le dire, mon moteur de recherche est du genre un peu
+                simplet, donc soyez pas trop compliqué dans votre choix de mot
+                pour ne pas trop le mêler! 😉
+              </p>
+            </div>
+          ) : (
+            <ProductList products={products} />
+          )}
+        </>
       </div>
     );
   }
