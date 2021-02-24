7b3d2a1ee19fd5fcaccf3398e5837eee5ddd075d
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2021-02-23 22:12:16 -0500

    Import product logic

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,14 +1,32 @@
 import "./ProductList.scss";
 
-import Product from "../Product/Product";
-
 const ProductList = ({ products }) => {
   return (
     <section className="product-list">
       <div className="grid">
-        {products.map((product) => (
-          <Product key={product.id} product={product} />
-        ))}
+        {products.map((product) => {
+          const { id, name, description, images, price } = product;
+          return (
+            <div key={id} className="product">
+              <img
+                className="image"
+                alt={name}
+                src={images[0].src}
+                loading="lazy"
+              />
+              <h2 className="name">{name}</h2>
+              <div
+                className="description"
+                dangerouslySetInnerHTML={{
+                  __html: description,
+                }}
+              ></div>
+              <div className="meta">
+                <div className="pricing">Semis : {price}$ ch.</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
