7e53d3afc010466fd94a67ad123b6addcf3474b4
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2021-02-26 22:49:00 -0500

    Rename link anchor

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -19,7 +19,7 @@ const Navbar = () => {
               <Link to="/">Accueil</Link>
             </li>
             <li>
-              <Link to="/produits">Nos semis</Link>
+              <Link to="/produits">Boutique</Link>
             </li>
           </ul>
         </div>
